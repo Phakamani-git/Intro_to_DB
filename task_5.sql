9e09eea801777bc9c321b66c698d56fdfb7855a2
Phakamani Sidimba <psidimba@gmail.com>
2025-06-25 19:28:03 +0200

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,4 +1,4 @@
 USE alx_book_store;
 
-INSERT INTO Customers (customer_id, customer_name, email, address)
+INSERT INTO Customer (customer_id, customer_name, email, address)
 VALUES (1, 'John Doe', 'johndoe@example.com', '123 Main Street');
